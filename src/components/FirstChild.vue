584f4c619eec3eaedeaac54402bda8262ba4ceec
RedLee <1091262281@qq.com>
2018-08-10 16:43:53 +0800

    提交代码

--- src/components/FirstChild.vue
+++ src/components/FirstChild.vue
@@ -1,12 +1,25 @@
 <template>
-  <h3>HelloWorld说{{ message }}</h3>
+  <div>
+    <h3>HelloWorld说{{ message }}</h3>
+    <el-button @click="countAdd">加一</el-button>
+    <h3>count is {{ count }}</h3>
+  </div>
 </template>
 <script>
+  import ElButton from "element-ui/packages/button/src/button";
   export default {
+    components: {ElButton},
     props:['message'],
-    date() {
+    data() {
       return {
-
+      }
+    },
+    computed: {
+      count: this.$store.state.test.count,
+    },
+    methods: {
+      countAdd() {
+        this.count++
       }
     }
   }
